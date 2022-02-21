3e1e8e5886f0952c1d86c7b554fa89aaa732ee6b
Jim Adamson <jim.adamson@york.ac.uk>
2022-02-21 15:46:36 +0000

    Add two .cmd files to serve as launchers for the script. Basically these files include parameterised presets (printer name, and Alma printer ID) for two use-cases in the UoY Library. Margins can and probably will be added to these files, once suitable margin values are determined.

---
+++ uoy-library-profiles/Interlending.cmd
@@ -0,0 +1,1 @@
+powershell -command "& { . ..\FetchAlmaPrint.ps1;Fetch-Jobs -printerId \""19195349880001381\"" -localPrinterName \""PUSH_ITSPRN0705 [Harry Fairhurst - Information Services LFA/ LFA023](Mobility)\"" }"
---
+++ uoy-library-profiles/epsonPOS.cmd
@@ -0,0 +1,6 @@
+powershell -command "& { . ..\FetchAlmaPrint.ps1;Fetch-Jobs -printerId \""848838010001381\"" -localPrinterName \""EPSON TM-T88III Receipt\"" }"
+
+
+
+
+
