a9b2fc7ad4fd67baa651806d0c67a6de2fbc844f
Jim Adamson <jim.adamson@york.ac.uk>
2022-02-22 18:12:40 +0000

    Polish IE11 first-time run helper script

--- extras/DisableFirstRunIE.ps1
+++ extras/DisableFirstRunIE.ps1
@@ -1,7 +1,13 @@
 #Requires -RunAsAdministrator
+#Requires -Version 4.0
 # Disable the 'Set up Internet Explorer 11' box - code snagged from https://stackoverflow.com/a/52985845/1754517
 $keyPath = 'Registry::HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Internet Explorer\Main'
+$valueName = 'DisableFirstRunCustomize'
+$valueData = 1
 if (!(Test-Path $keyPath)) {
-    New-Item $keyPath -Force
+  "Creating subkey(s)"
+  $null = New-Item $keyPath -Force
 }
-Set-ItemProperty -Path $keyPath -Name "DisableFirstRunCustomize" -Value 1
+"Creating $valueName value"
+Set-ItemProperty -Path $keyPath -Name $valueName -Value $valueData
+"Done"
