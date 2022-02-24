c4c35d378955bd7b506c650e493534278d9d4ee7
Jim Adamson <jim.adamson@york.ac.uk>
2022-02-24 10:46:08 +0000

    Improve launcher scripts: - set titlebar text to name of script + "Alma Printing With Powershell" - add powershell.exe options to hide the PS logo and minimise the window to the taskbar

--- uoy-library-profiles/Interlending.cmd
+++ uoy-library-profiles/Interlending.cmd
@@ -1,1 +1,3 @@
-powershell -command "& { . ..\FetchAlmaPrint.ps1;Fetch-Jobs -printerId \""19195349880001381\"" -localPrinterName \""PUSH_ITSPRN0705 [Harry Fairhurst - Information Services LFA/ LFA023](Mobility)\"" }"
+@echo off
+title %~n0 - Alma Printing With Powershell
+powershell -NoLogo -WindowStyle Minimized -command "& { . ..\FetchAlmaPrint.ps1;Fetch-Jobs -printerId \""19195349880001381\"" -localPrinterName \""PUSH_ITSPRN0705 [Harry Fairhurst - Information Services LFA/ LFA023](Mobility)\"" }"
--- uoy-library-profiles/epsonPOS.cmd
+++ uoy-library-profiles/epsonPOS.cmd
@@ -1,4 +1,6 @@
-powershell -command "& { . ..\FetchAlmaPrint.ps1;Fetch-Jobs -printerId \""848838010001381\"" -localPrinterName \""EPSON TM-T88III Receipt\"" }"
+@echo off
+title %~n0 - Alma Printing With Powershell
+powershell -NoLogo -WindowStyle Minimized -command "& { . ..\FetchAlmaPrint.ps1;Fetch-Jobs -printerId \""848838010001381\"" -localPrinterName \""EPSON TM-T88III Receipt\"" }"
 
 
 
