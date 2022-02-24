9845064f3ed60b19a586e6be45f08a0f11c13801
Jim Adamson <jim.adamson@york.ac.uk>
2022-02-24 10:56:39 +0000

    Further changes to launcher scripts: - adjust titlebar text to remind operators not to close the window - add '-checkInterval 15' to powershell line. In practice we'll want to avoid long waits for printouts to arrive, so we'll check more frequently than the default of 30 seconds

--- uoy-library-profiles/epsonPOS.cmd
+++ uoy-library-profiles/epsonPOS.cmd
@@ -1,6 +1,6 @@
 @echo off
-title %~n0 - Alma Printing With Powershell
-powershell -NoLogo -WindowStyle Minimized -command "& { . ..\FetchAlmaPrint.ps1;Fetch-Jobs -printerId \""848838010001381\"" -localPrinterName \""EPSON TM-T88III Receipt\"" }"
+title %~n0 - Alma Printing With Powershell - leave this window open or minimised to continue printing
+powershell -NoLogo -WindowStyle Minimized -command "& { . ..\FetchAlmaPrint.ps1;Fetch-Jobs -checkInterval 15 -printerId \""848838010001381\"" -localPrinterName \""EPSON TM-T88III Receipt\"" }"
 
 
 
