5e82b1a6b6595ae1db11a8cf1ff1d72cbbcb5d9b
Jim Adamson <jim.adamson@york.ac.uk>
2022-03-04 15:33:28 +0000

    Adjust marginTop for Interlending printing

--- uoy-library-profiles/Interlending.cmd
+++ uoy-library-profiles/Interlending.cmd
@@ -1,3 +1,3 @@
 @echo off
 title %~n0 - Alma Printing With Powershell - leave this window open or minimised to continue printing
-powershell -NoLogo -WindowStyle Minimized -command "& { . ..\FetchAlmaPrint.ps1;Fetch-Jobs -checkInterval 15 -printerId \""19195349880001381\"" -localPrinterName \""PUSH_ITSPRN0705 [Harry Fairhurst - Information Services LFA/ LFA023](Mobility)\"" }"
+powershell -NoLogo -WindowStyle Minimized -command "& { . ..\FetchAlmaPrint.ps1;Fetch-Jobs -checkInterval 15 -printerId \""19195349880001381\"" -localPrinterName \""PUSH_ITSPRN0705 [Harry Fairhurst - Information Services LFA/ LFA023](Mobility)\"" -marginTop \""0.155560\"" }"
